a1165a913ce90a7696fe02663884155ca7fda506
istrategy <developer@istrategytech.com>
2018-07-25 18:16:02 +0100

    dev

--- resources/views/pages/reports/index.blade.php
+++ resources/views/pages/reports/index.blade.php
@@ -29,7 +29,7 @@
                             <div class="card-block">
                                 <div class="row m-b-10">
                                     <div class="col-lg-3 col-sm-3 col-4">
-                                        <i class="icon-cart2 x6 text-alpha-2"></i>
+                                        <i class="icon-users2 x6 text-alpha-2"></i>
                                     </div>
                                     <div class="col-lg-9 col-sm-9 col-8">
                                         <div class="x4 text-light no-line-height text-alpha-9 text-right">463<i class="icon-arrow-up16 x2 text-alpha-6 position-right"></i></div>
@@ -105,7 +105,7 @@
                         <!-- Total profits -->
                         <div class="card card-inverse card-flat bg-danger">
                             <div class="card-header">
-                                <h5 class="text-uppercase text-semibold text-alpha-8">Total profits</h5>
+                                <h5 class="text-uppercase text-semibold text-alpha-8">Project Status</h5>
                             </div>
                             <div class="card-block">
                                 <div class="row m-b-10">
@@ -116,10 +116,10 @@
                                         <div class="x4 text-light no-line-height text-right"><small class="x2 position-left">$</small>8,420</div>
                                     </div>
                                 </div>
-                                <h6 class="text-alpha-7 float-left m-t-5 m-b-5">This month</h6>
+                                <h6 class="text-alpha-7 float-left m-t-5 m-b-5">Completed</h6>
                                 <h6 class="text-bold text-alpha-9 float-right m-t-5 m-b-5">$485</h6>
                                 <div class="clearfix"></div>
-                                <h6 class="text-alpha-7 float-left m-t-5 m-b-5">Last month</h6>
+                                <h6 class="text-alpha-7 float-left m-t-5 m-b-5">Active</h6>
                                 <h6 class="text-bold text-alpha-9 float-right m-t-5 m-b-5">$980</h6>
                                 <div class="clearfix"></div>
                             </div>
